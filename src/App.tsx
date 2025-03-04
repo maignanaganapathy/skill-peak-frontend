b92b318f677fbc55bb7a17514ecf9b868c3446e9
Karthika J <ripekarthika@gmail.com>
2025-03-05 00:50:34 +0530

    karthika|Landing page navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme"; // Import global theme
 import Landingpage from "./features/landing/Landingpage"; 
-import Login from "./features/auth/pages/Login";
-import SignUp from "./features/auth/pages/Signup";
+
 
 
 const App: React.FC = () => {
@@ -14,8 +13,6 @@ const App: React.FC = () => {
         <div className="flex flex-col min-h-screen">
           <Routes>
             <Route path="/" element={<Landingpage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
             
           </Routes>
         </div>
